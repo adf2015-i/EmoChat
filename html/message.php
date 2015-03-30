112f0363a707058571efb8d69413cf7916030244
elzzup <guild0105@gmail.com>
2015-03-30 12:57:08 +0900

    リクエスト受信処理

--- html/message.php
+++ html/message.php
@@ -3,6 +3,7 @@
  * user1, user2 ,message
  */
 require_once ('../data/config.php');
+require_once ('./EmotionRecogtion.php');
 $post = filter_input_array(INPUT_POST);
 try {
     $dbh = new PDO(DSN, DBUSER, DBPASSWORD);
@@ -12,18 +13,28 @@ catch(PDOException $e) {
     die();
 }
 
+// TODO: emotion 判定
+$date = date('Ymd-His');
+$file_name = "{$user_name}-{$date}.csv";
+$dirpath = "profile-iOS/";
+// if (!file_exists($dirpath)) {
+//     mkdir($dirpath, 0755, true);
+// }
+
+
 $sql = "SELECT * FROM chat_logs WHERE (user1= '{$post['user1']}' AND user2 = '{$post['user2']}') OR (user1= '{$post['user2']}' AND user2 = '{$post['user1']}')";
 $stmt = $dbh->query($sql);
 $res = $stmt->fetch();
 $arr = json_decode($res["log"] ?: array(),TRUE);
 $now = date("Y-m-d H:i:s");
-// TODO: emotion 判定
+
 $arr[] = array(
     "user_id"=>$post["user1"],
     "emotion"=>1,
     "message"=>$post["message"],
     "time"=>$now
 );
+// image path
 
 $json = json_encode($arr);
 $sql = "UPDATE chat_logs SET log={$json},last_date=NOW() WHERE id = ".$res['id'];
